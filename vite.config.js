671cff8546e2ad758b44b1ade9f0121c0adb1f33
Naresh-Front-dev <nareshiyer321@gmail.com>
2025-05-14 11:24:04 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,14 +3,5 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  server: {
-    proxy: {
-      '/api': {
-        target: 'https://backend-keiu.onrender.com',
-        changeOrigin: true,
-        rewrite: (path) => path.replace(/^\/api/, ''),
-        secure: false
-      }
-    }
-  }
+
 })
